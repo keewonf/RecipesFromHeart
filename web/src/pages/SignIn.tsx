0615ebcfb12fc00953b2390153f516d318d4a15c
keewonf <lukasfmm@gmail.com>
2026-05-15 18:15:17 -0300

    feat: add SignIn form with react-hook-form and zod validation

--- web/src/pages/SignIn.tsx
+++ web/src/pages/SignIn.tsx
@@ -1,3 +1,52 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Controller, useForm } from "react-hook-form";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+import { Link } from "react-router";
+
+const signInSchema = z.object({
+  email: z.email("E-mail inválido"),
+  password: z.string("Senha inválida").trim().min(1, "Informe a senha"),
+});
+
+type FormData = z.infer<typeof signInSchema>;
+
 export function SignIn() {
-  return <h1>SignIn</h1>;
+  const { control, handleSubmit } = useForm<FormData>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(signInSchema),
+  });
+
+  return (
+    <form>
+      <Controller
+        control={control}
+        name="email"
+        render={({ field }) => (
+          <Input required type="email" placeholder="email" {...field} />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name="password"
+        render={({ field }) => (
+          <Input required type="password" placeholder="senha" {...field} />
+        )}
+      />
+
+      <Button type="submit">Entrar</Button>
+      <Link className="text-surface-dark " to="/help">
+        Esqueci a senha
+      </Link>
+      <Link className="text-surface-dark" to="/signup">
+        Ainda não tem conta? Crie uma!
+      </Link>
+    </form>
+  );
 }
