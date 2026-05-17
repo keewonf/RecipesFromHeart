47e00c326fd2b6926e354d78d655de9926d2065f
keewonf <lukasfmm@gmail.com>
2026-05-17 13:52:22 -0300

    refactor: make AuthLayout title configurable

--- web/src/components/AuthLayout.tsx
+++ web/src/components/AuthLayout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router";
-import logoSvg from "../assets/logo.svg";
 
-export function AuthLayout() {
+type AuthLayoutProps = {
+  title: string;
+};
+
+export function AuthLayout({ title }: AuthLayoutProps) {
   return (
     <div className="w-screen min-h-screen bg-surface-light flex flex-col justify-center items-center text-text-primary ">
       <main className="p-8 rounded-md flex items-center flex-col md:min-w-115.5">
-        <img src={logoSvg} alt="logo" className="mb-8" />
+        <h1 className="mb-8 text-3xl font-bold text-text-secondary">{title}</h1>
         <Outlet />
       </main>
     </div>
--- web/src/routes/AuthRoutes.tsx
+++ web/src/routes/AuthRoutes.tsx
@@ -5,7 +5,7 @@ import { AuthLayout } from "../components/AuthLayout";
 export function AuthRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<AuthLayout />}>
+      <Route path="/" element={<AuthLayout title="Login" />}>
         <Route path="/" element={<SignIn />} />
       </Route>
     </Routes>
