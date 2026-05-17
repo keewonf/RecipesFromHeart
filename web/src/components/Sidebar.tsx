62bec397a5cbc6631c2ad62db44b84d9a3092bed
keewonf <lukasfmm@gmail.com>
2026-05-17 18:32:16 -0300

    refactor(sidebar): update sidebar spacing, colors, and navigation styles

--- web/src/components/Sidebar.tsx
+++ web/src/components/Sidebar.tsx
@@ -8,46 +8,64 @@ type SidebarProps = {
 };
 
 export function Sidebar({ className }: SidebarProps) {
-  const asideClass = classMerge("w-64 bg-white p-4", className);
-
   return (
-    <aside className={asideClass}>
-      <img src={logoSvg} alt="logo" />
+    <aside className={classMerge(["bg-surface-light-dark", className])}>
+      <div className="my-10 flex items-center justify-center">
+        <img src={logoSvg} alt="logo" />
+      </div>
 
-      <nav>
-        <ul>
+      <nav className="text-surface-dark font-bold text-xl">
+        <ul className="flex flex-col gap-7">
           <li>
-            <NavLink to="/">
+            <NavLink
+              to="/"
+              className="flex items-center gap-2 transition-all duration-200 hover:-translate-y-0.5 hover:text-text-primary"
+            >
               <Home />
               <span>Ínicio</span>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/recipes/user">
+            <NavLink
+              to="/recipes/user"
+              className="flex items-center gap-2 transition-all duration-200 hover:-translate-y-0.5 hover:text-text-primary"
+            >
               <Notebook />
               <span>Minhas receitas</span>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/recipes/community">
+            <NavLink
+              to="/recipes/community"
+              className="flex items-center gap-2 transition-all duration-200 hover:-translate-y-0.5 hover:text-text-primary"
+            >
               <Users />
               <span>Receitas da comunidade</span>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/recipes/favorites">
+            <NavLink
+              to="/recipes/favorites"
+              className="flex items-center gap-2 transition-all duration-200 hover:-translate-y-0.5 hover:text-text-primary"
+            >
               <Heart />
               <span>Favoritos</span>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/profile">
+            <NavLink
+              to="/profile"
+              className="flex items-center gap-2 transition-all duration-200 hover:-translate-y-0.5 hover:text-text-primary"
+            >
               <User />
               <span>Perfil</span>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/">
+            <NavLink
+              to="/"
+              className="flex items-center gap-2 transition-all duration-200 hover:-translate-y-0.5 hover:text-text-primary"
+            >
               <LogOut />
               <span>Sair</span>
             </NavLink>
