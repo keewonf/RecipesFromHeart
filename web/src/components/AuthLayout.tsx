c83e8784d19209aaa8c37856cfa864ce994129cf
keewonf <lukasfmm@gmail.com>
2026-05-17 14:26:05 -0300

    refactor: remove heading from auth layout

--- web/src/components/AuthLayout.tsx
+++ web/src/components/AuthLayout.tsx
@@ -1,14 +1,9 @@
 import { Outlet } from "react-router";
 
-type AuthLayoutProps = {
-  title: string;
-};
-
-export function AuthLayout({ title }: AuthLayoutProps) {
+export function AuthLayout() {
   return (
     <div className="w-screen min-h-screen bg-surface-light flex flex-col justify-center items-center text-text-primary ">
       <main className="p-8 rounded-md flex items-center flex-col md:min-w-115.5">
-        <h1 className="mb-8 text-3xl font-bold text-text-secondary">{title}</h1>
         <Outlet />
       </main>
     </div>
