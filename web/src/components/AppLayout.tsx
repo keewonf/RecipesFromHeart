45a13149885e9da5504f88282a3b19eb356aec51
keewonf <lukasfmm@gmail.com>
2026-05-18 17:40:05 -0300

    refactor: restructure app layout with header and outlet

--- web/src/components/AppLayout.tsx
+++ web/src/components/AppLayout.tsx
@@ -1,22 +1,14 @@
+import { Outlet } from "react-router";
+import { Header } from "./Header";
 import { Sidebar } from "./Sidebar";
 
 export function AppLayout() {
   return (
     <div className="flex min-h-screen flex-col bg-surface-light md:grid md:grid-cols-[minmax(150px,200px)_1fr] md:grid-rows-[auto_1fr]">
-      <Sidebar className="w-full border-b p-3 md:row-span-2 md:w-auto md:border-b-0 md:border-r" />
-      <header className="bg-surface-light p-3">
-        <button>Notificações</button>
-        <span>Imagem</span>
-      </header>
-      <main className="bg-surface-light p-3">
-        <h1>Minhas receitas</h1>
-        <div>
-          <h2>Cupcake</h2>
-          <p>Tempo: 1h10</p>
-          <p>12 porções</p>
-          <p>2 estrelas</p>
-        </div>
-      </main>
+      <Sidebar className="w-full border-b border-stone-300/80 p-3 md:row-span-2 md:w-auto md:border-b-0 md:border-r" />
+      <Header />
+
+      <Outlet />
     </div>
   );
 }
