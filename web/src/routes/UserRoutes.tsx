1e059af06fbf70d23ecb3f51e8e596dd7a8657d1
keewonf <lukasfmm@gmail.com>
2026-05-18 19:48:14 -0300

    feat(routes): add /recipes/new route and link from MyRecipes

--- web/src/routes/UserRoutes.tsx
+++ web/src/routes/UserRoutes.tsx
@@ -3,12 +3,14 @@ import { AppLayout } from "../components/AppLayout";
 
 import { NotFound } from "../pages/NotFound";
 import MyRecipes from "../pages/recipes/MyRecipes";
+import { CreateRecipe } from "../pages/recipes/CreateRecipe";
 
 export function UserRoutes() {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
-        <Route path="/" element={<MyRecipes />} />
+        <Route path="/recipes" element={<MyRecipes />} />
+        <Route path="/recipes/new" element={<CreateRecipe />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
