8a0c9072e8469cb162af95dafa687933a34057cc
keewonf <lukasfmm@gmail.com>
2026-05-18 19:49:14 -0300

    feat: add link for new recipe

--- web/src/pages/recipes/MyRecipes.tsx
+++ web/src/pages/recipes/MyRecipes.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import { RecipeList } from "../../components/recipes/RecipeList";
 import { useRecipes } from "../../hooks/useRecipes";
 
@@ -6,9 +7,18 @@ export function MyRecipes() {
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold text-text-primary mb-4">
-        Minhas receitas
-      </h1>
+      <div className="flex justify-between items-center mb-5">
+        <h1 className="text-2xl font-bold text-text-primary mb-4">
+          Minhas receitas
+        </h1>
+        <Link
+          to="/recipes/new"
+          className="flex h-12 items-center justify-center rounded-3xl border border-surface-dark bg-surface-dark p-3 text-xl text-white transition-colors duration-200 ease-linear hover:bg-text-primary"
+        >
+          Nova receita
+        </Link>
+      </div>
+
       <RecipeList recipes={recipes} loading={loading} onLoadMore={loadMore} />
     </div>
   );
