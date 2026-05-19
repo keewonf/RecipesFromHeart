201185cc913e34be01428ee1834f3657d9f2d70e
keewonf <lukasfmm@gmail.com>
2026-05-19 18:01:59 -0300

    feat: add default layout to community recipes

--- web/src/pages/recipes/CommunityRecipes.tsx
+++ web/src/pages/recipes/CommunityRecipes.tsx
@@ -2,16 +2,17 @@ import { RecipeList } from "../../components/recipes/RecipeList";
 import { useRecipes } from "../../hooks/useRecipes";
 
 export function CommunityRecipes() {
-  const { recipes, loading, loadMore } = useRecipes({ type: "community" });
+  const { recipes, loading, loadMore } = useRecipes({ type: "mine" });
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4 text-text-primary">
-        Receitas da comunidade
-      </h1>
+      <div className="flex justify-between items-center mb-5">
+        <h1 className="text-2xl font-bold text-text-primary mb-4">
+          Receitas da comunidade
+        </h1>
+      </div>
+
       <RecipeList recipes={recipes} loading={loading} onLoadMore={loadMore} />
     </div>
   );
 }
-
-export default CommunityRecipes;
