61192ad7525c5fabcdbae10905c2a59a184caba4
keewonf <lukasfmm@gmail.com>
2026-05-19 20:30:12 -0300

    feat: setup cors and errorHandling on app.ts

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import cors from "cors";
+import { errorHandling } from "./middlewares/error-handling";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(errorHandling)
 
 export { app };
