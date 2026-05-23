3ae82abbd2d38b515c295db756f3f444ae8a9398
keewonf <lukasfmm@gmail.com>
2026-05-22 22:01:13 -0300

    feat: add cloudinary configuration and env validation

--- api/src/env.ts
+++ api/src/env.ts
@@ -4,6 +4,15 @@ const envSchema = z.object({
   DATABASE_URL: z.url({ error: "DATABASE_URL inválida ou não definida" }),
   JWT_SECRET: z.string({ error: "Defina um JWT_SECRET" }).min(1),
   PORT: z.coerce.number().default(3333),
+  CLOUDINARY_CLOUD_NAME: z
+    .string({ error: "CLOUDINARY_CLOUD_NAME é obrigatório" })
+    .min(1),
+  CLOUDINARY_API_KEY: z
+    .string({ error: "CLOUDINARY_API_KEY é obrigatório" })
+    .min(1),
+  CLOUDINARY_API_SECRET: z
+    .string({ error: "CLOUDINARY_API_SECRET é obrigatório" })
+    .min(1),
 });
 
-export const env = envSchema.parse(process.env)
+export const env = envSchema.parse(process.env);
