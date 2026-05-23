d2cf67a36e0e25aa779748405e0e79d2de754d9d
keewonf <lukasfmm@gmail.com>
2026-05-23 08:24:39 -0300

    feat: add reusable button variants

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,19 +1,36 @@
+import { classMerge } from "../utils/classMerge";
+
 type Props = React.ComponentProps<"button"> & {
   isLoading?: boolean;
+  variant?: "base" | "icon" | "iconSmall";
+};
+
+const variants = {
+  button: {
+    base: "h-12 w-full",
+    icon: "h-12 w-12",
+    iconSmall: "h-8 w-8",
+  },
 };
 
 export function Button({
   children,
   isLoading,
   type = "button",
+  variant = "base",
+  className,
   ...rest
 }: Props) {
   return (
     <button
-      className="flex justify-center items-center bg-surface-dark
-      border border-surface-dark rounded-3xl w-full h-12 text-white text-xl cursor-pointer hover:bg-text-primary transition ease-linear p-3"
       type={type}
       disabled={isLoading}
+      className={classMerge([
+        "flex justify-center items-center bg-surface-dark border border-surface-dark rounded-3xl w-full h-12 text-white text-xl cursor-pointer hover:bg-text-primary transition ease-linear p-3",
+        variants.button[variant],
+        isLoading && "cursor-progress",
+        className,
+      ])}
       {...rest}
     >
       {isLoading ? "Carregando..." : children}
