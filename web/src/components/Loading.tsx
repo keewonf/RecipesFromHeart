0349bef35bbfb8664fa3a83f72171de8be65ef82
keewonf <lukasfmm@gmail.com>
2026-05-23 08:43:45 -0300

    feat: add loading state component

---
+++ web/src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export function Loading() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen w-screen bg-surface-light">
+      <h1 className="text-2xl font-bold">Carregando...</h1>
+    </div>
+  );
+}
