6046ef0ea1e068e566ff6e9c833e1e6651e23ec4
keewonf <lukasfmm@gmail.com>
2026-05-25 20:13:45 -0300

    feat: allow profile to reuse recipes list without header

--- web/src/pages/recipes/MyRecipes.tsx
+++ web/src/pages/recipes/MyRecipes.tsx
@@ -2,27 +2,30 @@ import { Link } from "react-router";
 import { RecipeList } from "../../components/recipes/RecipeList";
 import { useRecipes } from "../../hooks/useRecipes";
 
-export function MyRecipes() {
+type MyRecipesProps = {
+  showHeader?: boolean;
+};
+
+export function MyRecipes({ showHeader = true }: MyRecipesProps) {
   const { recipes, loading } = useRecipes({ type: "mine" });
 
   return (
     <div className="p-4">
-      <div className="flex justify-between items-center mb-5">
-        <h1 className="text-2xl font-bold text-text-primary mb-4">
-          Minhas receitas
-        </h1>
-        <Link
-          to="/recipes/new"
-          className="flex h-12 items-center justify-center rounded-3xl border border-surface-dark bg-surface-dark p-3 text-xl text-white transition-colors duration-200 ease-linear hover:bg-text-primary"
-        >
-          Nova receita
-        </Link>
-      </div>
+      {showHeader && (
+        <div className="mb-5 flex items-center justify-between">
+          <h1 className="mb-4 text-2xl font-bold text-text-primary">
+            Minhas receitas
+          </h1>
+          <Link
+            to="/recipes/new"
+            className="flex h-12 items-center justify-center rounded-3xl border border-surface-dark bg-surface-dark p-3 text-xl text-white transition-colors duration-200 ease-linear hover:bg-text-primary"
+          >
+            Nova receita
+          </Link>
+        </div>
+      )}
 
-      <RecipeList
-        recipes={recipes}
-        loading={loading}
-      />
+      <RecipeList recipes={recipes} loading={loading} />
     </div>
   );
 }
