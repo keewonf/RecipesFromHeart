a5dc0fc65bc8d7499ea65a451e15dc619a86f209
keewonf <lukasfmm@gmail.com>
2026-05-26 02:11:35 -0300

    refactor: change community endpoint to root recipes route

--- api/src/routes/recipes-routes.ts
+++ api/src/routes/recipes-routes.ts
@@ -6,7 +6,7 @@ const recipesController = new RecipesController();
 
 recipesRoutes.post("/", recipesController.create);
 recipesRoutes.get("/me", recipesController.index);
-recipesRoutes.get("/community", recipesController.community);
+recipesRoutes.get("/", recipesController.community);
 recipesRoutes.patch("/:id", recipesController.update);
 recipesRoutes.delete("/:id", recipesController.delete);
 
