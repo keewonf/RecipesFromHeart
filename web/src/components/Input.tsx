ca1ae4f339e4acb76927af253627002eaa7396e4
keewonf <lukasfmm@gmail.com>
2026-05-26 01:01:42 -0300

    feat: add error support to input component

--- web/src/components/Input.tsx
+++ web/src/components/Input.tsx
@@ -1,8 +1,9 @@
 type Props = React.ComponentProps<"input"> & {
   legend?: string;
+  error?: string;
 };
 
-export function Input({ legend, type = "text", ...rest }: Props) {
+export function Input({ legend, error, type = "text", ...rest }: Props) {
   return (
     <fieldset className="flex flex-1 text-surface-dark flex-col">
       {legend && (
@@ -12,11 +13,12 @@ export function Input({ legend, type = "text", ...rest }: Props) {
       )}
       <input
         type={type}
-        className={
-          "border border-gray-300 rounded-3xl px-4 py-3.75 w-full h-12 text-sm text-gray-600  outline-none focus:border-2 focus:border-surface-dark placeholder-gray-600/90 bg-white placeholder:uppercase font-bold"
-        }
+        className={`
+          "border border-gray-300 rounded-3xl px-4 py-3.75 w-full h-12 text-sm text-gray-600  outline-none focus:border-2 focus:border-surface-dark placeholder-gray-600/90 bg-white placeholder:uppercase font-bold ${error ? "border-red-500" : "border-gray-300 focus:border-surface-dark"}
+        }`}
         {...rest}
       />
+      <span className="text-red-500 text-xs min-h-4.5 block">{error}</span>
     </fieldset>
   );
 }
