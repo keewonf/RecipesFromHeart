c9a62afadbb53d14b9a35164498a0e9c3be05725
keewonf <lukasfmm@gmail.com>
2026-05-26 02:19:49 -0300

    feat: render routes based on authentication state

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,12 +1,31 @@
 import { BrowserRouter } from "react-router";
 import { AuthRoutes } from "./AuthRoutes";
 import { UserRoutes } from "./UserRoutes";
-
+import { Loading } from "../components/Loading";
+import { useAuth } from "../hooks/useAuth";
 
 export function Routes() {
+  const { session, isLoading } = useAuth();
+
+  function Route() {
+    switch (session?.user.role) {
+      case "ADMIN":
+        return <UserRoutes />;
+      case "MODERATOR":
+        return <UserRoutes />;
+      case "USER":
+        return <UserRoutes />;
+      default:
+        return <AuthRoutes />;
+    }
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <BrowserRouter>
-      <UserRoutes />
+      <Route />
     </BrowserRouter>
   );
 }
