9c1e5c18270d4fd9476cf6ce935b204e5f24b504
keewonf <lukasfmm@gmail.com>
2026-05-27 04:37:21 -0300

    feat: expose public home and auth routes

--- web/src/routes/AuthRoutes.tsx
+++ web/src/routes/AuthRoutes.tsx
@@ -2,16 +2,27 @@ import { Routes, Route } from "react-router";
 import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
 import { AuthLayout } from "../components/AuthLayout";
+import { PublicLayout } from "../components/PublicLayout";
 import { NotFound } from "../pages/NotFound";
 import { Help } from "../pages/Help";
+import { Home } from "../pages/Home";
+import { CommunityRecipes } from "../pages/recipes/CommunityRecipes";
 
 export function AuthRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<AuthLayout />}>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/help" element={<Help />} />
+      <Route path="/" element={<PublicLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/recipes/community" element={<CommunityRecipes />} />
+      </Route>
+      <Route path="/signin" element={<AuthLayout />}>
+        <Route index element={<SignIn />} />
+      </Route>
+      <Route path="/signup" element={<AuthLayout />}>
+        <Route index element={<SignUp />} />
+      </Route>
+      <Route path="/help" element={<AuthLayout />}>
+        <Route index element={<Help />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
