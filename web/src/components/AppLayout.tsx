230452cbead0d4d787ea87e64e1b1aef1637a70e
keewonf <lukasfmm@gmail.com>
2026-05-27 00:44:49 -0300

    chore: temporarily hide header component

--- web/src/components/AppLayout.tsx
+++ web/src/components/AppLayout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router";
-import { Header } from "./Header";
+//import { Header } from "./Header";
 import { Sidebar } from "./Sidebar";
 
 export function AppLayout() {
   return (
     <div className="flex min-h-screen flex-col bg-surface-light md:grid md:grid-cols-[minmax(150px,200px)_1fr] md:grid-rows-[auto_1fr]">
       <Sidebar className="w-full border-b border-stone-300/80 p-3 md:row-span-2 md:w-auto md:border-b-0 md:border-r" />
-      <Header />
+      {/* <Header /> */}
 
       <Outlet />
     </div>
