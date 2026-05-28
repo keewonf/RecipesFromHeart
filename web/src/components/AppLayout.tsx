b264c469d9b9759b9e279023cb01b4b078341fb0
keewonf <lukasfmm@gmail.com>
2026-05-28 08:33:27 -0300

    style: remove extra spacing and indentation

--- web/src/components/AppLayout.tsx
+++ web/src/components/AppLayout.tsx
@@ -7,7 +7,6 @@ export function AppLayout() {
     <div className="flex min-h-screen flex-col overflow-x-hidden bg-surface-light md:grid md:grid-cols-[minmax(160px,220px)_1fr] md:grid-rows-[auto_1fr]">
       <Sidebar className="w-full border-b border-stone-300/80 p-3 md:row-span-2 md:w-auto md:border-b-0 md:border-r md:p-4" />
       {/* <Header /> */}
-
       <Outlet />
     </div>
   );
